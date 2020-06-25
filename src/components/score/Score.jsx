58db4a8cf460669f8bc81a6c5a4391efb783575f
cgraves09 <christopher.graves09@gmail.com>
2020-06-24 20:53:04 -0400

    created subheader for game details

--- src/components/score/Score.jsx
+++ src/components/score/Score.jsx
@@ -4,7 +4,7 @@ import './score.css'
 const Score = props => (
         <div className="col-md-4 score">
             <div className="col-md-12">
-                <h1 className='subHeader'>{props.status ? props.status : 'Start the game by clicking on a image' }</h1>
+                <h1 className='subHeader'>{props.status ? props.status : 'Don\'t click on the same character twice.' }</h1>
             </div>
             <hr className='lineBreak'/>
             <div className="col-md-12">
--- src/components/subHeader/SubHeader.jsx
+++ src/components/subHeader/SubHeader.jsx
@@ -3,8 +3,12 @@ import './subHeader.css';
 const SubHeader = props => (
     <div className='container-fluid'>
         <div className="row">
-            <div className="col-md-12">
+            <div className="col-md-6">
                 <h1 className="logo">Clicky Game</h1>
+            </div>
+            <div className="col-md-6">
+                <h2 className='directions'>Start the game by clicking on a Character.</h2>
+                <hr className="lineBreak"/>
             </div>
         </div>           
     </div>
